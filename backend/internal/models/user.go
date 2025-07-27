38ecd27b35e30269f3ca60ff58d7a85788f26064
Vilaphong Douangmala <vilaphong.dml@gmail.com>
2025-07-28 02:35:30 +0700

    BE: added user model

---
+++ backend/internal/models/user.go
@@ -0,0 +1,8 @@
+package models
+
+type User struct {
+	ID           int    `json:"id"`
+	Email        string `json:"email" gorm:"unique"`
+	Password     string `json:"password"`
+	BuisnessName string `json:"buisness_name"`
+}
